4c90c8fc7e8ecd7fe5617408d30c0954d635f6c4
TwIxToR-TiTaN <bideblade1@hotmail.com>
2014-09-30 11:56:44 +0200

    2e Push
    Second build

--- js/script.js
+++ js/script.js
@@ -1,53 +1,12 @@
-$(document).ready(function() {
-   $('.0').mouseenter(function() {
-       $(this).animate({
-           'margin-top':'+=34px'
-       });
-   });
-   $('.0').mouseleave(function() {
-       $(this).animate({
-           'margin-top':'-=34px'
-       }); 
-   });
-      $('.1').mouseenter(function() {
-       $(this).animate({
-           'margin-top':'+=34px'
-       });
-   });
-   $('.1').mouseleave(function() {
-       $(this).animate({
-           'margin-top':'-=34px'
-       }); 
-   });
-      $('.2').mouseenter(function() {
-       $(this).animate({
-           'margin-top':'+=34px'
-       });
-   });
-   $('.2').mouseleave(function() {
-       $(this).animate({
-           'margin-top':'-=34px'
-       }); 
-   });
-      $('.3').mouseenter(function() {
-       $(this).animate({
-           'margin-top':'+=34px'
-       });
-   });
-   $('.3').mouseleave(function() {
-       $(this).animate({
-           'margin-top':'-=34px'
-       }); 
-   });
-   
-         $('.4').mouseenter(function() {
-       $(this).animate({
-           'margin-top':'+=34px'
-       });
-   });
-   $('.4').mouseleave(function() {
-       $(this).animate({
-           'margin-top':'-=34px'
-       }); 
-   });
+$(document).ready(function () {
+    $('#gamehead').mouseenter(function () {
+        $('#gameinfo').animate({
+            'margin-top': '+=40px'
+        });
+    });
+    $('#gamehead').mouseleave(function () {
+        $('#gameinfo').animate({
+            'margin-top': '-=40px'
+        });
+    });
 });
